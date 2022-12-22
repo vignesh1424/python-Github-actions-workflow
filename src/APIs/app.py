7cc3a1772ddf16a1583bf3d2d9ef49b77f2a5c0b
Vignesh.s <vignesh.settu@cibt.com>
2022-12-22 13:45:22 +0530

    python test

--- src/APIs/app.py
+++ src/APIs/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__names__)
 
 @app.route("/")
 def index():
     return "Hello, world!"
 
 
-if __name__ == "__main__:
+if __name__ == "__main__":
     app.run()
 
